f273fd27e5f6edf9c0f6b7b68e829fbc8b8808b2
Dvir <dvir.kafri@gmail.com>
2017-12-08 21:40:38 -0800

    Fix pop8 and type annotations.

--- src/tilemap.py
+++ src/tilemap.py
@@ -1,13 +1,18 @@
+from typing import List
+
 import pygame as pg
 import pytmx
-from settings import *
 
-def collide_hit_rect(one, two):
+from settings import TILESIZE, WIDTH, HEIGHT
+
+
+def collide_hit_rect(one: pg.sprite.Sprite, two: pg.sprite.Sprite) -> bool:
     return one.hit_rect.colliderect(two.rect)
 
+
 class Map:
-    def __init__(self, filename):
-        self.data = []
+    def __init__(self, filename: str) -> None:
+        self.data: List[str] = []
         with open(filename, 'rt') as f:
             for line in f:
                 self.data.append(line.strip())
@@ -17,14 +22,15 @@ class Map:
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
 
+
 class TiledMap:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         tm = pytmx.load_pygame(filename, pixelalpha=True)
         self.width = tm.width * tm.tilewidth
         self.height = tm.height * tm.tileheight
         self.tmxdata = tm
 
-    def render(self, surface):
+    def render(self, surface: pg.Surface) -> None:
         ti = self.tmxdata.get_tile_image_by_gid
         for layer in self.tmxdata.visible_layers:
             if isinstance(layer, pytmx.TiledTileLayer):
@@ -34,24 +40,25 @@ class TiledMap:
                         surface.blit(tile, (x * self.tmxdata.tilewidth,
                                             y * self.tmxdata.tileheight))
 
-    def make_map(self):
+    def make_map(self) -> pg.Surface:
         temp_surface = pg.Surface((self.width, self.height))
         self.render(temp_surface)
         return temp_surface
 
+
 class Camera:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.camera = pg.Rect(0, 0, width, height)
         self.width = width
         self.height = height
 
-    def apply(self, entity):
+    def apply(self, entity: pg.sprite.Sprite) -> None:
         return entity.rect.move(self.camera.topleft)
 
-    def apply_rect(self, rect):
+    def apply_rect(self, rect: pg.Rect) -> pg.Rect:
         return rect.move(self.camera.topleft)
 
-    def update(self, target):
+    def update(self, target: pg.sprite.Sprite) -> None:
         x = -target.rect.centerx + int(WIDTH / 2)
         y = -target.rect.centery + int(HEIGHT / 2)
 
