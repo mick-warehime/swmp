4b81feadb0762a91150eb6d79e8839175d83d61c
Miiick <mickwarehime@gmail.com>
2017-12-09 18:47:18 -0800

    add keybindings class

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from os import path
 import settings
 from sprites import Player, Mob, Obstacle, Item, collide_hit_rect
 import tilemap
+import controller as ctrl
 
 
 # HUD functions
@@ -57,6 +58,12 @@ class Game:
 
         self.all_sprites = LayeredUpdates()
         self._init_groups()
+
+        self.controller: ctrl.Controller = ctrl.Controller()
+        self.controller.set_binding(pg.K_ESCAPE, self.quit)
+        self.controller.set_binding(pg.K_n, self.toggle_night)
+        self.controller.set_binding(pg.K_h, self.toggle_debug)
+        self.controller.set_binding(pg.K_p, self.toggle_paused)
 
     def draw_text(self, text: str, font_name: str, size: int, color: tuple,
                   x: int, y: int, align: str = "topleft") -> None:
@@ -169,11 +176,6 @@ class Game:
                 self.update()
             self.draw()
 
-    @staticmethod
-    def quit() -> None:
-        pg.quit()
-        sys.exit()
-
     def update(self) -> None:
         # update portion of the game loop
         self.all_sprites.update()
@@ -264,19 +266,20 @@ class Game:
         # catch all events here
         for event in pg.event.get():
             if event.type == pg.QUIT:
+                print("i quit!")
                 self.quit()
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    self.quit()
-                if event.key == pg.K_h:
-                    self.draw_debug = not self.draw_debug
-                if event.key == pg.K_p:
-                    self.paused = not self.paused
-                if event.key == pg.K_n:
-                    self.night = not self.night
+                # check for any keys/clicks
+                self.controller.update()
 
-    def show_start_screen(self) -> None:
-        pass
+    def toggle_night(self) -> None:
+        self.night = not self.night
+
+    def toggle_debug(self) -> None:
+        self.draw_debug = not self.draw_debug
+
+    def toggle_paused(self) -> None:
+        self.paused = not self.paused
 
     def show_go_screen(self) -> None:
         self.screen.fill(settings.BLACK)
@@ -297,14 +300,19 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     waiting = False
+                    print("no i quit")
                     self.quit()
                 if event.type == pg.KEYUP:
                     waiting = False
 
+    @staticmethod
+    def quit() -> None:
+        pg.quit()
+        sys.exit()
+
 
 # create the game object
 g = Game()
-g.show_start_screen()
 while True:
     g.new()
     g.run()
