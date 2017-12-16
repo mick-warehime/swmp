81e100765c54eb258e2ef3e23452683c7a7c7762
Dvir <dvir.kafri@gmail.com>
2017-12-16 10:29:58 -0500

    Add lines to make coverage possible locally.

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -4,6 +4,7 @@ import sys, os
 
 sys.path.append('../../')
 sys.path.append('../')
+sys.path.append('.')
 import pygame
 from pygame.sprite import Group, LayeredUpdates
 import model, images, sounds
