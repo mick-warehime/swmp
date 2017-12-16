1b1a44e6bdf9b21171f8891010a51855767b9fe6
Dvir <dvir.kafri@gmail.com>
2017-12-16 09:38:14 -0500

    Attempt to fix display.set_mode in travis

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -18,7 +18,7 @@ pg = pygame_mock.Pygame()
 
 def setUpModule() -> None:
     pygame.display.set_mode((600, 400))
-    pygame.mixer.pre_init(44100, -16, 4, 2048)
+    # pygame.mixer.pre_init(44100, -16, 4, 2048)
     pygame.init()
     images.initialize_images()
     sounds.initialize_sounds()
