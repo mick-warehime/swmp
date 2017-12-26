85d46ef5f76c53bb06dcb96c082342005f9ebe89
Dvir <dvir.kafri@gmail.com>
2017-12-26 16:52:39 -0500

    Add grunt sound

--- src/sounds.py
+++ src/sounds.py
@@ -26,7 +26,8 @@ WEAPON_SOUNDS = {ObjectType.PISTOL: ['pistol.wav'],
                  ObjectType.SHOTGUN: ['shotgun.wav']}
 EFFECTS_SOUNDS = {'level_start': 'level_start.wav',
                   'health_up': 'health_pack.wav',
-                  'gun_pickup': 'gun_pickup.wav'}
+                  'gun_pickup': 'gun_pickup.wav',
+                  'grunt': 'grunt.wav'}
 
 
 class SoundEffects(object):
