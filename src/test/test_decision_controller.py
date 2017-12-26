68bd40ec69b023eefc05a310519da3ee0f3ebe8f
Miiick <mickwarehime@gmail.com>
2017-12-26 17:29:54 -0500

    fix issue #127 move all pygame mockery into the unittest class setup method to prevent separate test modules from interacting with one antoher when mocking the keybindings of the pygame module

---
+++ src/test/test_decision_controller.py
@@ -0,0 +1,50 @@
+import unittest
+import decision_controller
+from test import pygame_mock
+
+
+class DecisionControllerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        pg = pygame_mock.Pygame()
+        decision_controller.pg.mouse = pg.mouse
+        decision_controller.pg.key = pg.key
+
+    def test_set_option_0(self) -> None:
+        prompt = 'Do you go into the swamp?'
+        options = ['one', 'two', 'three']
+        dc = decision_controller.DecisionController(None, prompt, options)
+
+        key0 = decision_controller.pg.K_1
+        decision_controller.pg.key.pressed[key0] = 1
+
+        dc.update()
+
+        self.assertEqual(dc.choice, 1)
+
+    def test_set_option_1(self) -> None:
+        prompt = 'Do you go into the swamp?'
+        options = ['one', 'two', 'three']
+        dc = decision_controller.DecisionController(None, prompt, options)
+
+        key1 = decision_controller.controller.pg.K_2
+        decision_controller.pg.key.pressed[key1] = 1
+
+        dc.update()
+
+        self.assertEqual(dc.choice, 2)
+
+    def test_set_option_2(self) -> None:
+        prompt = 'Do you go into the swamp?'
+        options = ['one', 'two', 'three']
+        dc = decision_controller.DecisionController(None, prompt, options)
+
+        key2 = decision_controller.controller.pg.K_3
+        decision_controller.pg.key.pressed[key2] = 1
+
+        dc.update()
+
+        self.assertEqual(dc.choice, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
