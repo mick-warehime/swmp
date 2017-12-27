55fff64719d21807bfdd1e5c4725f46f86993d07
Miiick <mickwarehime@gmail.com>
2017-12-26 22:09:47 -0500

    cleanup quest - add comments

--- src/quest.py
+++ src/quest.py
@@ -8,22 +8,31 @@ COMPLETE = -1
 
 
 class Quest(object):
+    '''An object used to organize a sequence of dungeons which create a
+    quest. A quest is represented as a directed graph (DiGraph). Clients
+    should only ask a quest for the next dungeon and the quest object will
+    keep track of the internal state of the quest.'''
+
     def __init__(self,
                  screen: pg.Surface,
                  quit_func: Any) -> None:
         self._screen = screen
-        self._scene_graph = self.load_scene_graph()
+        self._quest_graph = self._create_quest()
         self._current = 0
         self._quit_func = quit_func
-        self._current_node = self.root_scene()
+        self._current_scene = self._root_scene()
 
-    def load_scene_graph(self) -> nx.DiGraph:
+    # temporary function for creating quests - just description + filename
+    def _create_quest(self) -> nx.DiGraph:
         g = nx.DiGraph()
-        g.add_edges_from([('goto.tmx', 'level1.tmx')])
+        first_scene = QuestScene('First scene', 'goto.tmx')
+        second_scene = QuestScene('Second scene', 'level1.tmx')
+        g.add_edges_from([(first_scene, second_scene)])
         return g
 
-    def root_scene(self) -> str:
-        g = self._scene_graph
+    # find the root of the graph (first scene)
+    def _root_scene(self) -> Any:
+        g = self._quest_graph
         root = [n for n, d in g.in_degree() if d == 0]
 
         error_message = 'graph should only have 1 root, found %d'
@@ -31,34 +40,51 @@ class Quest(object):
 
         return root[0]
 
+    # clients should call this method to get the next dungeon
+    # either to start a quest or to get the next scene
+    # returns a valid dungeon of COMPLETE if the quest is over
     def next_dungeon(self) -> DungeonController:
-        if not self._current_node:
+        if not self._current_scene:
             return COMPLETE
 
-        tiled_map_file = self._current_node
-        self.show_intro(tiled_map_file)
+        tiled_map_file = self._current_scene
+        self._show_intro(tiled_map_file.description)
 
-        dungeon = self.load_dungeon()
+        dungeon = self._create_dungeon()
 
-        self.update_node()
+        self._update()
 
         return dungeon
 
-    def load_dungeon(self) -> DungeonController:
-        return DungeonController(self._screen, self._current_node)
+    # load the dungeon given a file - eventually this will be replaced
+    # with a dungeon generation or load quests from files
+    def _create_dungeon(self) -> DungeonController:
+        map_file = self._current_scene.map_file
+        return DungeonController(self._screen, map_file)
 
-    def update_node(self) -> None:
-        neighbors = self._scene_graph.neighbors(self._current_node)
+    # determine the next scene to run and set that scene as current
+    # temporary - for now just grab the first neighbor of the current node
+    def _update(self) -> None:
+        neighbors = self._quest_graph.neighbors(self._current_scene)
         neighbors = list(neighbors)
         if len(neighbors) == 0:
-            self._current_node = None
+            self._current_scene = None
             return
 
-        self._current_node = neighbors[0]
+        self._current_scene = neighbors[0]
 
-    def show_intro(self, description: str) -> None:
+    # show the scene description - temporary - for now we just hardcode
+    # a description but eventually we should use this to describe all the
+    # hooks of the scene / dramatic question
+    def _show_intro(self, description: str) -> None:
         screen = self._screen
         options = ['continue']
         dc = DecisionController(screen, description, options)
         dc.bind(pg.K_ESCAPE, self._quit_func)
         dc.wait_for_decision()
+
+
+class QuestScene(object):
+    def __init__(self, description: str, map_file: str) -> None:
+        self.description = description
+        self.map_file = map_file
--- src/test/test_quest.py
+++ src/test/test_quest.py
@@ -3,19 +3,19 @@ import quest
 from typing import Any
 
 
-def show_intro(self: Any, filename: str) -> None:
+def _show_intro(self: Any, filename: str) -> None:
     pass
 
 
-def load_dungeon(self: Any) -> Any:
+def _create_dungeon(self: Any) -> Any:
     pass
 
 
 class QuestTest(unittest.TestCase):
     def setUp(self) -> None:
         self.Quest = quest.Quest
-        self.Quest.show_intro = show_intro
-        self.Quest.load_dungeon = load_dungeon
+        self.Quest._show_intro = _show_intro
+        self.Quest._create_dungeon = _create_dungeon
 
     def test_quest_init(self) -> None:
         q = self.Quest(None, None)
