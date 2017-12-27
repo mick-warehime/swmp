1392dfb1aff2c91171fb3bdf3868229ed2da0daf
Miiick <mickwarehime@gmail.com>
2017-12-26 22:14:30 -0500

    fix test

--- src/test/test_quest.py
+++ src/test/test_quest.py
@@ -23,7 +23,7 @@ class QuestTest(unittest.TestCase):
 
     def test_quest_update_node(self) -> None:
         q = self.Quest(None, None)
-        node_i = q._current_node
+        node_i = q._current_scene
         q.next_dungeon()
-        node_f = q._current_node
+        node_f = q._current_scene
         self.assertNotEqual(node_i, node_f)
