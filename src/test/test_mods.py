a87cfb514333e33762abd12b89b61a166a6fbeec
Miiick <mickwarehime@gmail.com>
2017-12-27 09:18:22 -0500

    put back the dummy audio

--- src/test/test_mods.py
+++ src/test/test_mods.py
@@ -5,6 +5,8 @@ from mods import Mod, PistolObject
 from src.test.pygame_mock import MockTimer, initialize_pygame, \
     initialize_gameobjects
 
+# needs to be here to prevent screen from loading
+import src.test.dummy_audio_video
 
 def setUpModule() -> None:
     initialize_pygame()
