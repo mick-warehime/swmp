adc7a0c6cb6f3891121f5f1d13997ae5f16e6e32
Miiick <mickwarehime@gmail.com>
2017-12-26 21:35:03 -0500

    mock a few more methods in Quest

--- src/test/test_quest.py
+++ src/test/test_quest.py
@@ -1,16 +1,29 @@
 import unittest
 import quest
+from typing import Any
+
+
+def show_intro(self: Any, filename: str) -> None:
+    pass
+
+
+def load_dungeon(self: Any) -> Any:
+    pass
 
 
 class QuestTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.Quest = quest.Quest
+        self.Quest.show_intro = show_intro
+        self.Quest.load_dungeon = load_dungeon
+
     def test_quest_init(self) -> None:
-        q = quest.Quest(None, None)
+        q = self.Quest(None, None)
         self.assertTrue(q is not None)
 
-    # def test_quest_update_node(self) -> None:
-    #     q = quest.Quest(None, None)
-    #     node_i = q._current_node
-    #     q.next_dungeon()
-    #     node_f = q._current_node
-    #     self.assertNotEqual(node_i, node_f)
-
+    def test_quest_update_node(self) -> None:
+        q = self.Quest(None, None)
+        node_i = q._current_node
+        q.next_dungeon()
+        node_f = q._current_node
+        self.assertNotEqual(node_i, node_f)
