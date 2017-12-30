f46cc76bad9efb35173840e622ac6f26e19fe993
Dvir <dvir.kafri@gmail.com>
2017-12-30 15:30:26 -0800

    Add ShootLaser ability.

--- src/items/laser_weapons.py
+++ src/items/laser_weapons.py
@@ -4,6 +4,8 @@ from pygame.surface import Surface
 from pygame.transform import rotate, scale
 
 import images
+import sounds
+from abilities import FireProjectile
 from weapons import Projectile
 
 
@@ -13,18 +15,33 @@ class LaserBolt(Projectile):
     damage: int = 100
 
     def __init__(self, pos: Vector2, direction: Vector2) -> None:
-        # Since the bold is long and wide, I am approximating its rect by a
-        # square.
         image = images.get_image(images.LASER_BOLT).copy()
         image = scale(image, (30, 3))
-        assert direction.is_normalized()
         angle = direction.angle_to(Vector2(0, 0))
         self._base_image = rotate(image, angle)
 
         super().__init__(pos, direction, False)
 
+        # Since the bold is long and wide, I am approximating its rect by a
+        # square.
         self._base_rect = Rect(0, 0, 10, 10)
 
     @property
     def image(self) -> Surface:
         return self._base_image
+
+
+class ShootLaser(FireProjectile):
+    _kickback = 0
+    _cool_down_time = 500
+    _spread = 10
+    _projectile_count = 5
+    _make_projectile = LaserBolt
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.uses_left = 1
+
+    def _fire_effects(self, origin: Vector2) -> None:
+        sounds.play('grunt')
+        self.uses_left -= 1
