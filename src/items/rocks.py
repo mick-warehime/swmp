d11aed9f856297815d7b60bdae32937ad40c608d
Dvir <dvir.kafri@gmail.com>
2018-01-11 21:01:21 -0800

    Implement RockMod from data.

--- src/items/rocks.py
+++ src/items/rocks.py
@@ -6,7 +6,8 @@ from pygame.math import Vector2
 
 from abilities import Ability, ProjectileAbilityData, FireProjectile
 import images
-from mods import ItemObject, Mod, ModLocation, Buffs, Proficiencies
+from mods import ItemObject, Mod, ModLocation, Buffs, Proficiencies, ModData, \
+    ModFromData
 from sounds import fire_weapon_sound
 from tilemap import ObjectType
 from projectiles import ProjectileData
@@ -17,26 +18,6 @@ class RockObject(ItemObject):
 
     def __init__(self, pos: Vector2) -> None:
         self._check_class_initialized()
-        mod = RockMod()
-
-        super().__init__(mod, pos)
-
-    @property
-    def image(self) -> pg.Surface:
-        image = images.get_image(images.ROCK)
-        return pg.transform.scale(image, self.rock_size)
-
-
-def throw_rock_effect(origin: Vector2) -> None:
-    fire_weapon_sound(ObjectType.ROCK)
-
-
-class RockMod(Mod):
-    loc = ModLocation.ARMS
-
-    def __init__(self, buffs: List[Buffs] = None,
-                 profs: List[Proficiencies] = None) -> None:
-        super().__init__(buffs, profs)
 
         projectile_data = ProjectileData(hits_player=False, damage=25,
                                          speed=250,
@@ -49,33 +30,18 @@ class RockMod(Mod):
                                              spread=2,
                                              fire_effects=[throw_rock_effect],
                                              finite_uses=True, uses_left=1)
+        mod_data = ModData(ModLocation.ARMS, ability_data, images.ROCK,
+                           images.ROCK, 'Rock', True)
 
-        self._ability = FireProjectile(ability_data)
+        mod = ModFromData(mod_data)
 
-    @property
-    def ability(self) -> Ability:
-        return self._ability
-
-    @property
-    def expended(self) -> bool:
-        return self._ability.uses_left <= 0
+        super().__init__(mod, pos)
 
     @property
-    def stackable(self) -> bool:
-        return True
+    def image(self) -> pg.Surface:
+        image = images.get_image(images.ROCK)
+        return pg.transform.scale(image, self.rock_size)
 
-    @property
-    def description(self) -> str:
-        uses_left = self._ability.uses_left
-        if uses_left == 1:
-            return 'Rock'
-        else:
-            return '%d rocks' % (uses_left,)
 
-    @property
-    def equipped_image(self) -> pg.Surface:
-        return images.get_image(images.ROCK)
-
-    @property
-    def backpack_image(self) -> pg.Surface:
-        return images.get_image(images.ROCK)
+def throw_rock_effect(origin: Vector2) -> None:
+    fire_weapon_sound(ObjectType.ROCK)
