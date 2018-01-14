36a40c87efab0dfe556b81d9dedcfebeb10c9fd2
Dvir <dvir.kafri@gmail.com>
2018-01-13 17:43:02 -0800

    Remove try: except. Keep only try.

--- src/data/input_output.py
+++ src/data/input_output.py
@@ -17,12 +17,9 @@ def print_rock_err_msg(*args: Any) -> None:
 
 def load_projectile_data(name: str) -> ProjectileData:
     with open(PROJECTILE_FILE, 'r') as stream:
-        try:
-            data = yaml.load(stream)
-            kwargs = data[name]
-            if 'drops_on_kill' in kwargs:
-                assert kwargs['drops_on_kill'] == 'RockObject'
-                kwargs['drops_on_kill'] = print_rock_err_msg
-            return ProjectileData(**kwargs)
-        except yaml.YAMLError as exc:
-            print(exc)
+        data = yaml.load(stream)
+        kwargs = data[name]
+        if 'drops_on_kill' in kwargs:
+            assert kwargs['drops_on_kill'] == 'RockObject'
+            kwargs['drops_on_kill'] = print_rock_err_msg
+        return ProjectileData(**kwargs)
