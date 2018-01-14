5abadae0e740fdc65230d163cf8a5f5528fb1d55
Dvir <dvir.kafri@gmail.com>
2018-01-13 21:30:12 -0800

    Format

--- src/data/abilities_io.py
+++ src/data/abilities_io.py
@@ -1,17 +1,13 @@
-from typing import Any
-
 import yaml
 
 from abilities import AbilityData, ProjectileAbilityData, \
     RegenerationAbilityData
-from projectiles import ProjectileData
 
 from src import __path__ as _src_path  # type: ignore
 
 _src_path = _src_path[0]
 
 _ABILITIES_FILE = _src_path + '/data/abilities.yml'
-
 
 # TODO(dvirk): The MuzzleFlash and fire sounds must be added back in later.
 _ability_constructors = {'fire_projectile': ProjectileAbilityData,
