f8168c88cce3025619c890e6d4996a8e6118d9ac
Dvir <dvir.kafri@gmail.com>
2018-01-20 19:52:10 -0800

    Revert "bugs"
    This reverts commit 778b0d36ab9c194d64fa9e137c11c531ba2df515.

--- src/dungeon_controller.py
+++ src/dungeon_controller.py
@@ -41,18 +41,95 @@ class DungeonController(controller.Controller):
         self._view = view.DungeonView(self._screen)
         self._view.set_groups(self._groups)
 
-        self._init_controls()
+        self.init_controls()
 
         self.teleported = False
+
+    def _init_map_objects(self) -> None:
+        # provide the group containers for the map objects
+        self._init_gameobjects()
+        self._conflicts = ConflictGroups()
+
+        # initialize the player on the map before anything else
+        for obj in self._map.objects:
+            if obj.type == tilemap.ObjectType.PLAYER:
+                self.player = Player(obj.center)
+
+        assert self.player is not None, 'no player found in map'
+
+        for obj in self._map.objects:
+            conflict_group = self._get_conflict(obj.conflict)
+            if obj.type == tilemap.ObjectType.ZOMBIE:
+                Mob(obj.center, self.player, conflict_group)
+            if obj.type == tilemap.ObjectType.WALL:
+                pos = Vector2(obj.x, obj.y)
+                Obstacle(pos, obj.width, obj.height)
+            if obj.type in tilemap.ITEMS:
+                ItemManager.item(obj.center, obj.type)
+            if obj.type == tilemap.ObjectType.WAYPOINT:
+                Waypoint(obj.center, self.player, conflict_group)
+
+    def _get_conflict(self, conflict_name: str) -> Group:
+        if conflict_name == tilemap.NOT_CONFLICT:
+            return None
+        return self._conflicts.get_group(conflict_name)
+
+    def _init_gameobjects(self) -> None:
+        GameObject.initialize_gameobjects(self._groups)
+        timer = Timer(self)
+        DynamicObject.initialize_dynamic_objects(timer)
+        Mob.init_class(self._map.img)
+        abilities.initialize_classes(timer)
+
+    def init_controls(self) -> None:
+
+        self.bind_on_press(pg.K_n, self._view.toggle_night)
+        self.bind_on_press(pg.K_h, self._view.toggle_debug)
+
+        # players controls
+        self.bind(pg.K_LEFT, self.player.translate_left)
+        self.bind(pg.K_a, self.player.translate_left)
+
+        self.bind(pg.K_RIGHT, self.player.translate_right)
+        self.bind(pg.K_d, self.player.translate_right)
+
+        self.bind(pg.K_UP, self.player.translate_up)
+        self.bind(pg.K_w, self.player.translate_up)
+
+        self.bind(pg.K_DOWN, self.player.translate_down)
+        self.bind(pg.K_s, self.player.translate_down)
+
+        arms_ability = self.player.ability_caller(mods.ModLocation.ARMS)
+        self.bind(pg.K_SPACE, arms_ability)
+        self.bind_mouse(controller.MOUSE_LEFT, arms_ability)
+
+        chest_ability = self.player.ability_caller(mods.ModLocation.CHEST)
+        self.bind_on_press(pg.K_r, chest_ability)
+
+        self.bind_on_press(pg.K_b, self.toggle_hide_backpack)
+
+        # equip / use
+        self.bind_on_press(pg.K_e, self.try_equip)
+
+        self.bind_on_press(pg.K_t, self.teleport)
+
+    def draw(self) -> None:
+        pg.display.set_caption("{:.2f}".format(self.get_fps()))
+
+        self._view.draw(self.player, self._map, self._map.img, self._camera)
+
+        self._view.draw_conflicts(self._conflicts)
+
+        pg.display.flip()
 
     def update(self) -> None:
 
         # needs to be called every frame to throttle max framerate
         self.dt = self._clock.tick(settings.FPS) / 1000.0
 
-        self._pass_mouse_pos_to_player()
+        self.pass_mouse_pos_to_player()
 
-        clicked_hud = self._try_handle_hud()
+        clicked_hud = self.try_handle_hud()
         if not clicked_hud:
             self.handle_input()
 
@@ -110,78 +187,7 @@ class DungeonController(controller.Controller):
     def game_over(self) -> bool:
         return self.player.health <= 0
 
-    def resolved_conflict_index(self) -> int:
-        return self._conflicts.resolved_conflict()
-
-    def _init_map_objects(self) -> None:
-        # provide the group containers for the map objects
-        self._init_gameobjects()
-        self._conflicts = ConflictGroups()
-
-        # initialize the player on the map before anything else
-        for obj in self._map.objects:
-            if obj.type == tilemap.ObjectType.PLAYER:
-                self.player = Player(obj.center)
-
-        assert self.player is not None, 'no player found in map'
-
-        for obj in self._map.objects:
-            conflict_group = self._get_conflict(obj.conflict)
-            if obj.type == tilemap.ObjectType.ZOMBIE:
-                Mob(obj.center, self.player, conflict_group)
-            if obj.type == tilemap.ObjectType.WALL:
-                pos = Vector2(obj.x, obj.y)
-                Obstacle(pos, obj.width, obj.height)
-            if obj.type in tilemap.ITEMS:
-                ItemManager.item(obj.center, obj.type)
-            if obj.type == tilemap.ObjectType.WAYPOINT:
-                Waypoint(obj.center, self.player, conflict_group)
-
-    def _get_conflict(self, conflict_name: str) -> Group:
-        if conflict_name == tilemap.NOT_CONFLICT:
-            return None
-        return self._conflicts.get_group(conflict_name)
-
-    def _init_gameobjects(self) -> None:
-        GameObject.initialize_gameobjects(self._groups)
-        timer = Timer(self)
-        DynamicObject.initialize_dynamic_objects(timer)
-        Mob.init_class(self._map.img)
-        abilities.initialize_classes(timer)
-
-    def _init_controls(self) -> None:
-
-        self.bind_on_press(pg.K_n, self._view.toggle_night)
-        self.bind_on_press(pg.K_h, self._view.toggle_debug)
-
-        # players controls
-        self.bind(pg.K_LEFT, self.player.translate_left)
-        self.bind(pg.K_a, self.player.translate_left)
-
-        self.bind(pg.K_RIGHT, self.player.translate_right)
-        self.bind(pg.K_d, self.player.translate_right)
-
-        self.bind(pg.K_UP, self.player.translate_up)
-        self.bind(pg.K_w, self.player.translate_up)
-
-        self.bind(pg.K_DOWN, self.player.translate_down)
-        self.bind(pg.K_s, self.player.translate_down)
-
-        arms_ability = self.player.ability_caller(mods.ModLocation.ARMS)
-        self.bind(pg.K_SPACE, arms_ability)
-        self.bind_mouse(controller.MOUSE_LEFT, arms_ability)
-
-        chest_ability = self.player.ability_caller(mods.ModLocation.CHEST)
-        self.bind_on_press(pg.K_r, chest_ability)
-
-        self.bind_on_press(pg.K_b, self._toggle_hide_backpack)
-
-        # equip / use
-        self.bind_on_press(pg.K_e, self._try_equip)
-
-        self.bind_on_press(pg.K_t, self._teleport)
-
-    def _try_handle_hud(self) -> bool:
+    def try_handle_hud(self) -> bool:
         pos = self.get_clicked_pos()
         if pos == controller.NOT_CLICKED:
             return False
@@ -191,11 +197,11 @@ class DungeonController(controller.Controller):
 
         return self._view.clicked_hud(pos)
 
-    def _try_equip(self) -> None:
-        self._equip_mod_in_backpack()
-        self._unequip_mod()
+    def try_equip(self) -> None:
+        self.equip_mod_in_backpack()
+        self.unequip_mod()
 
-    def _equip_mod_in_backpack(self) -> None:
+    def equip_mod_in_backpack(self) -> None:
         '''equip amod if the user selects it in the backpack and hits the
         'equip' button binding.'''
         idx = self._view.selected_item()
@@ -207,7 +213,7 @@ class DungeonController(controller.Controller):
             self.player.equip(backpack[idx])
             self._view.set_selected_item(view.NO_SELECTION)
 
-    def _unequip_mod(self) -> None:
+    def unequip_mod(self) -> None:
         '''unequip amod if the user selects it in the hud and hits the
                 'equip' button binding.'''
         location = self._view.selected_mod()
@@ -216,21 +222,24 @@ class DungeonController(controller.Controller):
 
         self.player.unequip(location)
 
-    def _pass_mouse_pos_to_player(self) -> None:
-        mouse_pos = self._abs_mouse_pos()
+    def pass_mouse_pos_to_player(self) -> None:
+        mouse_pos = self.abs_mouse_pos()
         self.player.set_mouse_pos(mouse_pos)
 
     # mouse coordinates are relative to the camera
     # most other coordinates are relative to the map
-    def _abs_mouse_pos(self) -> Tuple[int, int]:
+    def abs_mouse_pos(self) -> Tuple[int, int]:
         mouse_pos = pg.mouse.get_pos()
         camera_pos = self._camera.rect
         abs_mouse_x = mouse_pos[0] - camera_pos[0]
         abs_mouse_y = mouse_pos[1] - camera_pos[1]
         return (abs_mouse_x, abs_mouse_y)
 
-    def _toggle_hide_backpack(self) -> None:
+    def toggle_hide_backpack(self) -> None:
         self._view.toggle_hide_backpack()
 
-    def _teleport(self) -> None:
+    def resolved_conflict_index(self) -> int:
+        return self._conflicts.resolved_conflict()
+
+    def teleport(self) -> None:
         self.teleported = True
