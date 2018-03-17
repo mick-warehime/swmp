c70d6d2aa0273ce49971dac261fbe63b8b946885
Dvir <dvir.kafri@gmail.com>
2018-03-17 15:24:30 -0700

    Cleaned up construction of Resolutions.

--- src/quests/resolutions.py
+++ src/quests/resolutions.py
@@ -1,7 +1,7 @@
 """Possible resolutions to dramatic questions."""
 import abc
 from enum import Enum
-from typing import Dict, Any, Collection
+from typing import Dict, Any, Collection, List, Callable
 
 from pygame.sprite import Group, Sprite, spritecollide
 
@@ -15,11 +15,22 @@ class ResolutionType(Enum):
     CONDITION = 'condition'
     DECISION_CHOICE = 'decision choice'
 
+    @property
+    def arg_labels(self) -> List[str]:
+        return _resolution_args[self]
+
+    @property
+    def constructor(self) -> Callable[Any, Resolution]:
+        return _resolution_map[self]
+
 
 SpriteLabels = Dict[str, Collection[Sprite]]
 
 
 class Resolution(abc.ABC):
+    """Represents a resolution to a Scene, which signals a change to the next.
+    """
+
     @property
     def is_resolved(self) -> bool:
         raise NotImplementedError
@@ -29,12 +40,14 @@ class Resolution(abc.ABC):
         """Called after sprites have been created and categorized from map."""
 
 
-def add_sprites_of_label(label: str, res_data: SpriteLabels,
-                         group: Group) -> None:
+def _add_sprites_of_label(label: str, res_data: SpriteLabels,
+                          group: Group) -> None:
     group.add(*res_data[label])
 
 
 class KillGroup(Resolution):
+    """Resolves when a group is killed."""
+
     def __init__(self, group_label: str) -> None:
         self._group_label = group_label
         self._group_to_kill = Group()
@@ -44,11 +57,13 @@ class KillGroup(Resolution):
         return len(self._group_to_kill) == 0
 
     def load_sprite_data(self, sprite_categories: SpriteLabels) -> None:
-        add_sprites_of_label(self._group_label, sprite_categories,
-                             self._group_to_kill)
+        _add_sprites_of_label(self._group_label, sprite_categories,
+                              self._group_to_kill)
 
 
 class EnterZone(Resolution):
+    """Resolves when a labeled sprite enters a labeled zone."""
+
     def __init__(self, zone_label: str, entering_label: str) -> None:
         self._zone_label = zone_label
         self._entering_label = entering_label
@@ -61,16 +76,19 @@ class EnterZone(Resolution):
                    sprite in self._entering_group)
 
     def load_sprite_data(self, sprite_categories: SpriteLabels) -> None:
-        add_sprites_of_label(self._zone_label, sprite_categories,
-                             self._zone_group)
-        add_sprites_of_label(self._entering_label, sprite_categories,
-                             self._entering_group)
+        _add_sprites_of_label(self._zone_label, sprite_categories,
+                              self._zone_group)
+        _add_sprites_of_label(self._entering_label, sprite_categories,
+                              self._entering_group)
 
 
 class ConditionSatisfied(Resolution):
-    def __init__(self, tested_label: str, condition: Condition) -> None:
+    """Resolves when a Condition is satisfied on a given sprite."""
+
+    def __init__(self, tested_label: str,
+                 condition_data: Dict[str, Any]) -> None:
         self._label = tested_label
-        self._condition = condition
+        self._condition = condition_from_data(condition_data, None, None)
         self._tested: GameObject = None
 
     def load_sprite_data(self, sprite_categories: SpriteLabels) -> None:
@@ -87,6 +105,8 @@ class ConditionSatisfied(Resolution):
 
 
 class MakeDecision(Resolution):
+    """Resolves when the player chooses the described option."""
+
     def __init__(self, description: str) -> None:
         self.description = description
         self._decision_chosen = False
@@ -105,27 +125,26 @@ class MakeDecision(Resolution):
         return 'MakeDecision: {}'.format(self.description)
 
 
+_resolution_args = {ResolutionType.DECISION_CHOICE: ['description'],
+                    ResolutionType.CONDITION:
+                        ['tested label', 'condition data'],
+                    ResolutionType.ENTER_ZONE:
+                        ['zone label', 'entering label'],
+                    ResolutionType.KILL_GROUP: ['group label']}
+
+_resolution_map = {ResolutionType.DECISION_CHOICE: MakeDecision,
+                   ResolutionType.CONDITION: ConditionSatisfied,
+                   ResolutionType.ENTER_ZONE: EnterZone,
+                   ResolutionType.KILL_GROUP: KillGroup}
+
+
 def resolution_from_data(res_data: Dict[str, Any]) -> Resolution:
+    """Constructs a Resolution from a data dictionary."""
     assert len(res_data.keys()) == 1, 'root keys must be the length one, ' \
                                       'matching the resolution label.'
     res_label = list(res_data.keys())[0]
     data = res_data[res_label]
     res_type = ResolutionType(res_label)
 
-    if res_type == ResolutionType.KILL_GROUP:
-        group_label = data['group label']
-        return KillGroup(group_label)
-    elif res_type == ResolutionType.ENTER_ZONE:
-        zone_label = data['zone label']
-        entering_label = data['entering label']
-        return EnterZone(zone_label, entering_label)
-    elif res_type == ResolutionType.CONDITION:
-        condition_data = data['condition data']
-        condition = condition_from_data(condition_data, None, None)
-        tested_label = data['tested label']
-        return ConditionSatisfied(tested_label, condition)
-    elif res_type == ResolutionType.DECISION_CHOICE:
-        return MakeDecision(data['description'])
-    else:
-        raise NotImplementedError('Definition of resolution type {} not yet '
-                                  'implemented'.format(res_type))
+    args = [data[key] for key in res_type.arg_labels]
+    return res_type.constructor(*args)
